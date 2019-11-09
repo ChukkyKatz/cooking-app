4ce8c9f0ea6f7bf9e89eb03afb331ea922263a62
Chukky Katz <chukkykatz@gmail.com>
2019-11-10 01:29:55 +0200

    Error page added

---
+++ src/main/java/com/chukkykatz/cooking/controller/AppErrorController.java
@@ -0,0 +1,28 @@
+package com.chukkykatz.cooking.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AppErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(final HttpServletRequest request, final Model model) {
+        final Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        final String statusCode = status.toString();
+        model.addAttribute("scCh1", statusCode.charAt(0));
+        model.addAttribute("scCh2", statusCode.charAt(1));
+        model.addAttribute("scCh3", statusCode.charAt(2));
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
